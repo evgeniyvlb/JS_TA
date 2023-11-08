23ee400a3c7ba7034bb39c2dfc3832cec92a473b
Yevhen Plakhotnikov <su160985pev@gmail.com>
2023-11-08 11:44:54 -0800

    First lesson

---
+++ index.js
@@ -0,0 +1,61 @@
+// #2
+// console.log("Plakhotnikov");
+
+//#3
+// let a
+// let b
+
+// a = 5;
+// alert(`a: ${a}`);
+// b = a;
+// alert(`b: ${b}`);
+
+//#4
+// let obj = {
+//     String: "Yevhen",
+//     Number: 38,
+//     Boolean: false,
+//     Undefined: undefined,
+//     Null: null
+// };
+
+//#5
+// function confirm() {
+//    return prompt("What's the age of majority")
+// };
+
+// let isAdult = confirm()
+// console.log(isAdult);
+
+//#6
+// let name = "Yevhen"
+// let lastName = "Plahotnikov";
+// let studyGroup = "test"
+// let birthYear = 1985
+// let married = true
+
+// console.log(birthYear, married, name, lastName, studyGroup);
+
+// let age;
+// let status = null;
+
+// console.log(typeof age)
+// console.log(typeof status)
+
+//#7
+// (function confirm() {
+//     login = prompt("What's the login")
+//     email = prompt("What's the email")
+//     password = prompt("What's the password")
+//     console.log(`Dear ${login}, your email is ${email}, your password is ${password} .`)
+//     })()
+
+//#8
+// (function calculation(){
+//     secInHour = 60*60
+//     secInDay = secInHour*24
+//     secInMOnth= secInDay*30
+//     alert (secInHour);
+//     alert (secInDay);
+//     alert (secInMOnth);
+// })()
